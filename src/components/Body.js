f5622c13ffc2a58ef2c6f43cad5cd06a9df00db8
Anura Mohaniya <anura.mohaniya@gmail.com>
2023-07-04 14:51:17 +0530

    static page of food app

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,33 @@
 import React from 'react';
 
+//javascript object
+const styleCard = {
+    backgroundColor: "#f0f0f0"
+}
+
 const RestaurantCard = () => {
     return (
-        <div className="res-card">
-            <h3>Anura foods</h3>
-            
+        <div className="res-card" style= {styleCard}>
+        <img className="res-logo" alt= "res-logo" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ufrgm69mnqbvn36q6snp" />
+            <h3>Jenith Fast Food</h3>
+            <h4>Biryani, Pizza</h4>
+            <h4>ratings:3</h4>
         </div>
     );
 }
 
-1
 const Body = () => {
   return (
     <div className='body'>
         <div className='search'>Search</div>
         <div className="res-container">
             {/* restaurant card */}
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
             <RestaurantCard />
         </div>
     </div>
